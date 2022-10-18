4aca44676fd674197e35335a00ae3074505ecdec
xiao-mac <543033627@qq.com>
2022-10-18 16:51:50 +0800

    hystrix dashboard图形化界面对服务监控

---
+++ cloud-netflix/cloud-hystrix-dashboard9001/src/test/java/com/xiao/cloud/cloudhystrixdashboard9001/CloudHystrixDashboard9001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiao.cloud.cloudhystrixdashboard9001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudHystrixDashboard9001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
