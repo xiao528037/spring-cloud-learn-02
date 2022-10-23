3259a65861b93709ceef5ebe4982694da05b9156
xiao-mac <543033627@qq.com>
2022-10-23 15:44:38 +0800

    添加alibaba-cloud 添加服务提供者9001

---
+++ cloud-alibaba/cloud-alibaba-provider-payment9001/src/main/java/com/xiao/alibaba/cloud/cloudalibabaproviderpayment8001/CloudAlibabaProviderPayment8001Application.java
@@ -0,0 +1,13 @@
+package com.xiao.alibaba.cloud.cloudalibabaproviderpayment8001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAlibabaProviderPayment8001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAlibabaProviderPayment8001Application.class, args);
+	}
+
+}
