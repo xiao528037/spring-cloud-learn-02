9adc62b799abf8ad42ed332a6bca7f6e9e335f39
xiao-mac <543033627@qq.com>
2022-10-30 13:29:10 +0800

    添加seata-order模块

---
+++ cloud-alibaba/cloud-alibaba-seata-consumer-order80/src/main/java/com/xiao/cloud/cloudalibabaseataorder80/CloudAlibabaSeataOrder80Application.java
@@ -0,0 +1,13 @@
+package com.xiao.cloud.cloudalibabaseataorder80;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAlibabaSeataOrder80Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAlibabaSeataOrder80Application.class, args);
+	}
+
+}
